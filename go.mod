226823144acbdd87104f200ab0f349012ebbce12
Danny Carrillo <odannyc@users.noreply.github.com>
2021-05-13 14:50:22 -0700

    First PR - Repo setup (#1)
    * Adds README
    
    * Adds Goreleaser
    
    * Adds LICENSE
    
    * Adds editorconfig
    
    * Adds gitignore
    
    * Adds settings.yml
    
    * Adds Github Actions CI & Release

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/tunein/envoygrace
 
-go 1.15
+go 1.16
--- main.go
+++ main.go
@@ -10,13 +10,19 @@ import (
 )
 
 func main() {
-	var envoyTarget string
-	var evictionPeriodMS int
-	var envoyRequestTimeoutMS int
+	var (
+		envoyTarget string
+		evictionPeriodMS int
+		envoyRequestTimeoutMS int
+		preSleepMS int
+	)
 	flag.StringVar(&envoyTarget, "-envoy-url", "localhost:8001", "the base url where envoy lives")
 	flag.IntVar(&evictionPeriodMS, "-eviction-period", 5000, "amount of milliseconds to sleep after calling envoy graceful commands")
 	flag.IntVar(&envoyRequestTimeoutMS, "-envoy-timeout", 1000, "envoy request timeout in milliseconds")
+	flag.IntVar(&preSleepMS, "-pre-sleep", 5000, "how long to sleep before sending requests to envoy in milliseconds")
 	flag.Parse()
+
+	<-time.After(time.Duration(preSleepMS) * time.Millisecond)
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(envoyRequestTimeoutMS)*time.Millisecond)
 	defer cancel()
